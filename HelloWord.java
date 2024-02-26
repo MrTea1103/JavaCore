de4bbb239743802e23ec81a8978659ad93e85b82
MrTea1103 <hoainam441785@gmail.com>
2024-02-26 16:07:25 +0700

    init project

---
+++ HelloWord.java
@@ -0,0 +1,10 @@
+package learnJavaCore;
+
+public class HelloWord {
+	public static void main(String[] args) {
+		System.out.println("Hello Word!!!!");
+		System.out.println("Hello Word!!!!");
+		System.out.println("Hello Word!!!!");
+		System.out.println("Hello Word!!!!");
+	}
+}
